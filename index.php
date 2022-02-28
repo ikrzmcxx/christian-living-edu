87efcc43d825dfb7178ec5a6053e6863f389c0d9
ikrzmcxx <99166161+ikrzmcxx@users.noreply.github.com>
2022-02-28 16:14:03 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head> <title> Performance task in cle </head> </title>
+<body style="background-color:lightgreen;">
+
+<h1 style="font-size:300%;"> Performance Task In Christian Living Education </h1>
+      
+ <p> <h1 style="font- size:150%;"> <p style="font-family:courier;"> <p> This is the life-giving message we heard him share and it’s still ringing in our ears. We now repeat his words to you: God is pure light. You will never find even a trace of darkness in him.” 1 John 1:5 </h1> </p>
+<p><p> <h1 style="font- size:150%;"> <p style="font-family:courier;"> With God anything is possible. </h1> </p>
+   <p> Advocacy paper: </p>
+<a href="https://drive.google.com/file/d/11m2XxlQCA2JR3k0lkkk_Jb1FwlNxI5ds/view?usp=sharing"> click here </a>
+</body>
+</html>
